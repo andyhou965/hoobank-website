9f42c628f7649b55ac1ffaf41408365a3cba155c
AndyHou <andyhou965@gmail.com>
2024-05-13 13:46:23 +0400

    finish the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,52 @@
+import { useState } from "react";
+import { logo, close, menu } from "../assets";
+import { navLinks } from "../constants";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="flex items-center justify-between w-full py-6 navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+      <ul className="items-center justify-end flex-1 hidden list-none sm:flex">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center justify-end flex-1 sm:hidden">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute mx-4 top-20 right-0 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="flex flex-col items-center justify-end list-none">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
