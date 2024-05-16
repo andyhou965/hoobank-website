2494dc2a966e5e84c4a2be27924a05bde08a3c3e
AndyHou <andyhou965@gmail.com>
2024-05-16 08:04:34 +0400

    finish Features section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,12 @@
-const Button = () => {
-  return <div>Button</div>;
+// eslint-disable-next-line react/prop-types
+const Button = ({ styles }) => {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 };
 export default Button;
